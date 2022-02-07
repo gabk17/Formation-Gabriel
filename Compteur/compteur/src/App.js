4dffb36965c077444e05c7395567a33706c7c062
Gabriel <gabkanaan17@gmail.com>
2022-02-07 21:03:43 +0200

    added List Counter with ADD and REMOVE buttons

--- Compteur/compteur/src/App.js
+++ Compteur/compteur/src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 
-const App = () => {
-
+function Counter () {
+  
 const [counter, setCounter] = useState(0)
+const [counterList, setCounterList] = useState([])
 
+ 
 const increment = () => {
 	setCounter(counter + 1)
 }
@@ -16,17 +18,37 @@ const reset = () => {
   setCounter(0)
 }
 
+const addCounter = () => {
+  setCounterList(counterList.concat(<Counter id = {counterList.length  + 1} />))
+}
+
+const deleteCounter = (id) => {
+  setCounterList((counters) => counters.filter((_, i) => i !== counters.length - 1))
+}
+
+return(
+  <div>
+	  Here's A Counter: {counter}
+	  <div>
+		  <button onClick={increment}>Increment</button>
+		  <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={addCounter}>Add Counter</button>
+      <button onClick={deleteCounter}>Delete Counter</button>
+
+      {counterList}
+	  </div>
+	</div>
+)
+}
+
+const App = () => {
+
 return (
 	<div>
-	Here's A Counter: {counter}
-	<div>
-		<button 
-		onClick={increment}>Increment</button>
-		<button
-		onClick={decrement}>Decrement</button>
-    <button
-		onClick={reset}>Reset</button>
-	</div>
+
+    <Counter />
+
 	</div>
 )
 }
