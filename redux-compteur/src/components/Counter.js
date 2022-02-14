fcc0f38b25483130cf6e0b52b805af03a9345a09
Gabriel <gabkanaan17@gmail.com>
2022-02-14 14:28:29 +0200

    Added Reset Button

--- redux-compteur/src/components/Counter.js
+++ redux-compteur/src/components/Counter.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { increment, decrement } from '../actions/CounterActions';
+import { increment, decrement, reset } from '../actions/CounterActions';
 
 class Counter extends React.Component {
   render() {
-    const { value, onIncrementClick, onDecrementClick } = this.props;
+    const { value, onIncrementClick, onDecrementClick, onResetClick } = this.props;
 
     return (
       <div>
-        <span>{value}</span>
-        <button type="button" onClick={onIncrementClick}>+</button>
-        <button type="button" onClick={onDecrementClick}>-</button>
+        <div>
+          <span>{value}</span>
+        </div>
+        
+        <button type="button" onClick={onIncrementClick}>Increment</button>
+        <button type="button" onClick={onDecrementClick}>Decrement</button>
+        <button type="button" onClick={onResetClick}>Reset</button>
       </div>
     );
   }
@@ -23,6 +27,7 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   onIncrementClick: () => dispatch(increment()),
   onDecrementClick: () => dispatch(decrement()),
+  onResetClick: () => dispatch(reset()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
