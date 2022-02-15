26d4bc0c321c9f83e5d799f6aebd6ff009dedb71
Gabriel <gabkanaan17@gmail.com>
2022-02-15 10:54:18 +0200

    Used moment js to Format Date

--- redux-compteur/src/App.js
+++ redux-compteur/src/App.js
@@ -1,11 +1,17 @@
 import Counter from './components/Counter'
 import ReduxForm from './components/ReduxForm'
+import moment from 'moment'
 
 function App() {
+  
   const submit = values => {
-    // print the form values to the console
-    console.log(values)
+    const aDate = values.date
+    console.log(aDate);
+    console.log(JSON.stringify(aDate));
+    console.log(JSON.stringify(moment(aDate).format()));
+    console.log(JSON.stringify(moment(aDate).format('dddd')));
   }
+
   return (
     <div className="App">
       <Counter /> <br/>
