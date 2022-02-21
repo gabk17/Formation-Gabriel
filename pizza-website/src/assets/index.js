87fac34c319fab9e54689a1dbaaa5a42fe27321f
Gabriel <gabkanaan17@gmail.com>
2022-02-21 13:43:01 +0200

    Removed Unused Images

--- pizza-website/src/assets/index.js
+++ pizza-website/src/assets/index.js
@@ -2,8 +2,6 @@ export const image1 = require('./img1.png')
 export const image2 = require('./img2.png')
 export const image3 = require('./img3.png')
 export const background = require('./background.png')
-export const illustration1 = require('./illustration1.png')
-export const illustration2 = require('./illustration2.png')
 export const illustration3 = require('./sketchyPizza.png')
 
 
