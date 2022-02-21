000a794f44049f997e9606403b81a22b2545baae
Gabriel <gabkanaan17@gmail.com>
2022-02-21 13:46:38 +0200

    Replaced return statement in CardItem with parentheses

--- pizza-website/src/components/CardItem.js
+++ pizza-website/src/components/CardItem.js
@@ -1,23 +1,21 @@
 import Card from 'react-bootstrap/Card';
 import { NavLink } from 'react-router-dom';
 
-const CardItem = (item) => {
+const CardItem = (item) =>
+(
+  <Card key={item.id} style={{ width: '25rem', marginBottom: '3rem' }}>
+    <Card.Img style={{ marginTop: '1rem' }} variant="top" src={item.img} alt={item.title} />
+    <Card.Body>
+      <Card.Title>{item.title}, ${item.price}</Card.Title>
+      <Card.Text className="description">
+        {item.size}
+      </Card.Text>
+      <div className="d-grid gap-2">
+        <NavLink to={`/menu/${item.id}`} className="btn btn-outline-danger">Check Item Details</NavLink>
+      </div>
+    </Card.Body>
+  </Card>
+);
 
-    return (
-      <Card key={item.id} style={{ width: '25rem', marginBottom: '3rem' }}>
-        <Card.Img style={{ marginTop: '1rem' }} variant="top" src={item.img} alt={item.title} />
-        <Card.Body>
-          <Card.Title>{item.title}, ${item.price}</Card.Title>
-          <Card.Text className="description">
-            {item.size}
-          </Card.Text>
-          <div className="d-grid gap-2">
-            <NavLink to={`/menu/${item.id}`} className="btn btn-outline-danger">Check Item Details</NavLink>
-          </div>
-        </Card.Body>
-      </Card>
-    );
-  }
 
-  export default CardItem;
-  
+export default CardItem;
