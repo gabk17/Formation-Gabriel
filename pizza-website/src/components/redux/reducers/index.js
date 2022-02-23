9c8eb2eff23143ec2648e13a00854c65944093a0
Gabriel <gabkanaan17@gmail.com>
2022-02-23 16:45:12 +0200

    Remmoved old reducers import

--- pizza-website/src/components/redux/reducers/index.js
+++ pizza-website/src/components/redux/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
 
-import pizzaReducer from "./pizzaReducer";
-import orderReducer from "./orderReducer";
 import pizzaReducerSauce from "../reduxsauce/pizzaReducerSauce.js";
 import orderReducerSauce from "../reduxsauce/orderReducerSauce.js";
 
 const rootReducer = combineReducers({
   pizza: pizzaReducerSauce,
-  order : orderReducerSauce
+  order: orderReducerSauce
 });
 
 export default rootReducer;
